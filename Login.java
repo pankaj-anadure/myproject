6b08fcb0b33e5390835085dcc2de4e354d46a42b
pankaj-anadure <pankaj.anadure@wissen.com>
2022-01-11 09:15:01 +0530

    Change Password functionality added...

--- Login.java
+++ Login.java
@@ -1,5 +1,9 @@
 public class Login
 {
+	public void changePassword()
+	{
+		System.out.println("Change Password implementation");
+	}
 	public void login()
 	{
 		System.out.println("Enter username & password");
