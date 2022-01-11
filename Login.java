9c089ec892cc11c62f6cea50a7ea240f7f4737e4
pankaj-anadure <pankaj.anadure@wissen.com>
2022-01-11 09:20:17 +0530

    Modified Login.java

--- Login.java
+++ Login.java
@@ -1,7 +1,15 @@
 public class Login
 {
+	public void changePassword()
+	{
+		System.out.println("Implement change password logic");
+	}
 	public void login()
 	{
 		System.out.println("Enter username & password");
+	}
+	public void reSendOTP()
+	{
+		System.out.println("Generate and Send new OTP");
 	}
 }
